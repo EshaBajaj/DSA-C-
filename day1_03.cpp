4819fc9281ac775f6550f78791c01dea6d690632
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-06 23:20:13 +0530

    Updated file after push

--- day1_03.cpp
+++ day1_03.cpp
@@ -1,17 +1,19 @@
+#include <vector>
+using namespace std;
+
 class Solution {
-    public:
-        int countPairs(vector<int>& nums, int target) {
-            int count = 0;
-            int n = nums.size();
-    
-            for (int i = 0; i < n; ++i) {
-                for (int j = i + 1; j < n; ++j) {
-                    if (nums[i] + nums[j] < target) {
-                        count++; 
-                    }    
-                }
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int count = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] < target) {
+                    count++; 
+                }    
             }
-            return count;
         }
-    };
-    
+        return count;
+    }
+};
