bec893bb5a962735f6db1835490d3b1103e424a4
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-08 09:10:51 +0530

    Rotate an array k times

--- day2_02.cpp
+++ day2_02.cpp
@@ -1,17 +1,37 @@
+#include <iostream>
 #include <vector>
+#include <algorithm>                                           // for reverse
+
 using namespace std;
+
 class Solution {
-    public:
-        bool check(vector<int>& nums) {
-            int drops = 0;
-            int n = nums.size();
-    
-            for (int i = 0; i < n; i++) {
-                if (nums[i] > nums[(i + 1) % n]) {
-                    drops++;
-                }
-            }
-            return drops <= 1;
-        }
-    };
-    
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        // Reverse the entire array
+        reverse(nums.begin(), nums.end());
+
+       // Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+
+        // Reverse the remaining n-k elements
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    sol.rotate(nums, k);
+
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
