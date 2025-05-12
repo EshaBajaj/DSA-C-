26ddf54ecc6c67ba3f7d8ecab86c65dcfac9216d
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-12 18:32:23 +0530

    Triplets Find Redundant

---
+++ day5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+  public:
+    int countTriplets(vector<int> nums) {
+        // Code here.
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 0; i < n - 2; ++i) {
+            for (int j = i + 1; j < n - 1; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    if (nums[i] < nums[j] && nums[j] < nums[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
