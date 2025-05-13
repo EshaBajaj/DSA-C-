4396e82b0c1abf7ab79880cded78ab96759768b0
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-13 23:24:17 +0530

    MinorChanges

--- day5_01.cpp
+++ day5_01.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 class Solution{
 public:
-vector<int> runningSum(vector<int>& nums) {
+vector<int> runningSum(vector<int>& nums)
+{
 for (int i = 1; i < nums.size(); i++) {
             nums[i] += nums[i - 1]; 
         }
