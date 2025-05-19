d9815bcbb91942cccdf6bc4c0cd2ef54f4e9938a
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-19 19:12:18 +0530

    matrices

---
+++ day8.cpp
@@ -0,0 +1,12 @@
+class Matrix(int m, int n){
+  int m = mat.size(), n = mat[0].size();
+    vector<vector<int>> res(n, vector<int>(m));
+    for(int i=0; i<m; i++){
+        for(int i=0; i<n ; ++j){
+            cout<< matrix[i][j]<<" ";
+        }
+        cout << endl;
+    }
+}
+
+//
