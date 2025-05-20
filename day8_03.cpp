c77997ad31cc7a5067da4e740d111455321889f2
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-20 23:52:22 +0530

    Correction in leaders

--- day8_03.cpp
+++ day8_03.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 int main(){
     int c=0;
-    int N= ar.size()-1;
+    int N= vector<int>. size();
     for(int i=0; i<N; i++){
         int max=INT_MIN;
         for(int j=i+1; j<N; j++){
