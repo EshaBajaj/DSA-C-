7154a9c175f4029b5995ffd9d371b640a008d255
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-20 23:51:03 +0530

    leaders in an array

---
+++ day8_03.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+int main(){
+    int c=0;
+    int N= ar.size()-1;
+    for(int i=0; i<N; i++){
+        int max=INT_MIN;
+        for(int j=i+1; j<N; j++){
+            if(ar[j]>max){
+                max=a[j];
+            }
+        }
+        if(ar[j]>max){
+            c++;
+        }
+
+    }
+return 0;
+}
