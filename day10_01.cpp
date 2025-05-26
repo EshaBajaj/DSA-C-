aaef2f9d9574da2d188e6b20f04ad763d8dea93f
EshaBajaj <eshabajaj1626@gmail.com>
2025-05-26 19:40:21 +0530

    BuySell

---
+++ day10_01.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<vector>
+#include<climits>
+using namespace std;
+int main(){
+    int price={2,7,5,8,10,12};
+    int minPrice = price[0];
+    int maxPrice = 0;
+
+    for(int i=0; i<N; ++i){
+        if(price[i] < minPrice){
+            minPrice=price;
+        }
+        
+    }
+
+}
