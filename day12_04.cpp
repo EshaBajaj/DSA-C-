76415c1d760e2aa7d32fc26062841f8503f183db
EshaBajaj <eshabajaj1626@gmail.com>
2025-06-02 02:16:23 +0530

    RowSum

---
+++ day12_04.cpp
@@ -0,0 +1,15 @@
+lass Solution {
+  public:
+    vector<int> rowSum(vector<vector<int>>& mat) {
+        vector<int>result;
+        
+        for(int i=0; i<mat.size(); ++i){
+            int sum=0;
+            for(int j=0; j<mat[i].size(); j++){
+                sum+=mat[i][j];
+            }
+            result.push_back(sum);
+        }
+        return result;
+    }
+};
