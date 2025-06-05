826d96ad6924a0ef5e288cc17d43442b2d0b9d9c
EshaBajaj <eshabajaj1626@gmail.com>
2025-06-05 13:09:20 +0530

    SmallChanges

--- day13_01.cpp
+++ day13_01.cpp
@@ -12,8 +12,7 @@ class Solution{
     int getOddOccurrence(int arr[], int n) {
         int result = 0;
         for (int i = 0; i < n; i++) {
-            result ^= arr[i];  // XOR all elements
-        }
+            result ^= arr[i];
         return result;
     }
 };
